a5d7e07913fd3dad581413c817caa53dcfabe978
viki24681357 <39785913+viki24681357@users.noreply.github.com>
2018-06-05 21:46:00 -0400

    unit test added

--- spark/src/main/java/spark/poc/Enrich.scala
+++ spark/src/main/java/spark/poc/Enrich.scala
@@ -5,7 +5,7 @@ import org.apache.spark.rdd.RDD
 object Enrich {
   
   def wordCount(dataset:RDD[String], delimiter:String) :RDD[(String,Int)] = {
-    dataset.flatMap(_.split(delimiter)).map(a=>(a,1)).reduceByKey(_+_, 1)
+    dataset.flatMap(_.split(delimiter)).map(a=>(a,1)).reduceByKey(_+_, 1).sortByKey(true, 4)
   }
   
   def wordCountGreaterThanInput(dataset:RDD[String], delimiter:String,lowerLimit:Int) : RDD[(String,Int)] = {
