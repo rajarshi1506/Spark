1228ab237ba8fbb050350ea471a7e35bf7d7ad2b
rajarshi1506 <39785913+rajarshi1506@users.noreply.github.com>
2018-09-14 00:14:25 -0400

    more changes

--- src/MergeLogsFromKafka.scala
+++ src/MergeLogsFromKafka.scala
@@ -8,31 +8,33 @@ import org.apache.spark.streaming.dstream.DStream
 import kafka.serializer.Decoder
 import kafka.serializer.StringDecoder
 
-
 object MergeLogsFromKafka {
   def main(args: Array[String]): Unit = {
 
+    println("69.73.176.abj,2017-06-30,10:19:39,0.0,1567864.0,0001179110-17-009773,-index.htm,301.0,659.0,1.0,0.0,0.0,10.0,0.0".split(",")(0))
+
     val kafkaParams = Map[String, String](
       "bootstrap.servers" -> "localhost:9092",
-      "key.deserializer" -> "StringDeserializer",
-     "value.deserializer" -> "StringDeserializer",
+      //"key.deserializer" -> "StringDeserializer",
+      //"value.deserializer" -> "StringDeserializer",
       "group.id" -> "use_a_separate_group_id_for_each_stream",
-      "auto.offset.reset" -> "smallest"
-      ,"enable.auto.commit" -> "false"
-      )
+      "auto.offset.reset" -> "largest"
+    //,"enable.auto.commit" -> "false"
+    )
 
     val topics = Set("test")
-    
+
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("KAFKA STREAMING")
     val ssc = new StreamingContext(sparkConf, Seconds(10))
-    
-    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder,StringDecoder](ssc, kafkaParams, topics)
-    
-    kafkaStream.flatMap(_._2.split(" ")).map(x=>(x,1)).reduceByKey(_+_).foreachRDD(_.foreach(println))
-    
+
+    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
+
+    //get the count of 1st and second columns only
+    kafkaStream.map(x => x._2.split(",")(0)+","+x._2.split(",")(1)).flatMap(_.split(",")).map(x => (x, 1)).reduceByKey(_ + _).foreachRDD(_.take(100).foreach(println))
+
     ssc.start()
-    
+
     ssc.awaitTermination()
-    
+
   }
 }
