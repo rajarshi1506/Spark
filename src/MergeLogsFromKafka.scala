bc4aac3ee41c673f5c44a752030e0f350f6bc584
rajarshi1506 <39785913+rajarshi1506@users.noreply.github.com>
2019-02-08 17:27:25 -0500

    poc with multiple topics

--- src/MergeLogsFromKafka.scala
+++ src/MergeLogsFromKafka.scala
@@ -7,11 +7,15 @@ import java.util.HashMap
 import org.apache.spark.streaming.dstream.DStream
 import kafka.serializer.Decoder
 import kafka.serializer.StringDecoder
+import org.apache.spark.streaming.kafka.OffsetRange
+import org.apache.spark.streaming.kafka.HasOffsetRanges
 
 object MergeLogsFromKafka {
   def main(args: Array[String]): Unit = {
+    
+    // Hold a reference to the current offset ranges, so it can be used downstream
+    var offsetRanges = Array[OffsetRange]()
 
-    println("69.73.176.abj,2017-06-30,10:19:39,0.0,1567864.0,0001179110-17-009773,-index.htm,301.0,659.0,1.0,0.0,0.0,10.0,0.0".split(",")(0))
 
     val kafkaParams = Map[String, String](
       "bootstrap.servers" -> "localhost:9092",
@@ -23,14 +27,29 @@ object MergeLogsFromKafka {
     )
 
     val topics = Set("test")
+    //val topics = Set("test","test_partitioned","test_partitioned_4")
 
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("KAFKA STREAMING")
     val ssc = new StreamingContext(sparkConf, Seconds(10))
 
     val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
 
-    //get the count of 1st and second columns only
-    kafkaStream.map(x => x._2.split(",")(0)+","+x._2.split(",")(1)).flatMap(_.split(",")).map(x => (x, 1)).reduceByKey(_ + _).foreachRDD(_.take(100).foreach(println))
+    kafkaStream.transform { rdd =>
+   offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+   rdd
+ }
+    kafkaStream.transform{rdd=>
+      offsetRanges=rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd
+      }
+    .map(x=>x._2).flatMap(_.split(" ")).map(x=>(x,1)).reduceByKey(_+_)
+    .foreachRDD{rdd=>
+      for(offset <- offsetRanges){
+        println("partition::"+offset.partition+"   topic:::"+offset.topic+" from offset:::"+offset.fromOffset+" until offset::"+offset.untilOffset)
+      }
+      println("partitions in rdd::"+rdd.partitions.size)
+      rdd.foreach(println)
+    }
 
     ssc.start()
 
